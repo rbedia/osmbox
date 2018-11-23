eb8eefe8831be792c7754a3466b9435406218ddd
Rafael Bedia <dcat@trillinux.org>
2018-11-23 09:45:56 -0500

    Delete unneeded images and scripts.

--- imposm/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-exec /opt/imposm3/imposm3 \
-	"$@" \
-	-connection "postgis://${POSTGRESQL_USER}:${POSTGRESQL_PASSWORD}@db/${POSTGRESQL_DATABASE}" \
-	-mapping "/etc/imposm/mapping.yml"
-
